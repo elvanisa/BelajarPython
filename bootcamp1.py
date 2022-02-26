3dbafc85130e2d2e2084433076ecc8a8e570d566
Elvanisa <elvanisa.ayu@hreasily.com>
2022-02-26 07:17:07 +0700

    pertemuan 1

---
+++ bootcamp1.py
@@ -0,0 +1,44 @@
+# basic I/O
+# input()
+# output print()
+
+#format
+print('saya suka {0} dan {2}'.format('buah','sayur','permen'))
+
+buah = "mangga"
+sayur = "selada"
+print('buah yang saya suka adalah {} dan {}'.format(buah,sayur))
+
+"""
+ini comment panjang
+jadi bisa tulis apa aja disini
+"""
+
+#input
+print("halo binar")
+print("nama kamu siapa?")
+nama = input()
+print("nama aku adalah", nama)
+print("pekerjaan kamu apa?")
+job = input()
+print("pekerjaan aku adalah", job)
+usia = input("berapa umur kamu:")
+print("ohh umur kamu ternyata", usia, "tahun")
+
+# belajar angka
+a = 1
+print("\n nilai a adalah", a)
+print(a+a+a)
+a = 2
+print(a,a,a,a, sep="b", end="c")
+print(a,a, end="berakhir")
+
+print(a)
+a = 3
+a = 4
+a = 5
+print(a-1)
+
+#coba import
+import math
+print(math.pi)
